31096ee0eb98e51b7a0c2e2c27e5f2a180c15661
somratcste <somratcste@gmail.com>
2016-09-22 10:00:01 +0600

    Create view

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -2,11 +2,38 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 use App\Http\Requests;
+use App\Note;
+use Illuminate\Foundation\Http\response;
+use Illuminate\Http\Request;
 
 class NoteController extends Controller
 {
-    //
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+    	Note::create($request->all());
+    	return response()->json(['success' => 'Note Created Successfully']);
+    }
+
+    public function show($id)
+    {
+
+    }
+
+    public function edit($id)
+    {
+
+    }
+
+
 }
